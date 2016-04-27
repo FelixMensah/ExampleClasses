02a63a2a7b6b833eac80078e0eae5ac56260b6fa
FelixMensah <felix.mensah-adjei@outlook.fr>
2016-04-27 20:37:31 +0000

    modification of the string method

--- politician.py
+++ politician.py
@@ -1,23 +1,24 @@
 __author__ = 'Felix'
-class Politician:
 
+
+class Politician:
     politicianCount = 0
-    def __init__(self,firstname,lastname,country,mandat):
+
+    def __init__(self, firstname, lastname, country, mandate):
         self.firstname = firstname
         self.lastname = lastname
         self.country = country
-        self.mandat = mandat
+        self.mandate = mandate
         Politician.politicianCount += 1
 
     def displayPolitician(self):
-        print("The Head of State of " + self.country+", " + self.firstname.capitalize()+" "+self.lastname +
-        " has been elected for " + str(self.mandat) + " years.")
+        print("The Head of State of %s, %s %s has been elected for %d years." % (
+            self.country, self.firstname, self.lastname, self.mandate))
 
 
-
-Obama = Politician("Barack","Obama","USA",4)
-Merkel = Politician("Angela","Merkel","Germany",6)
-Hollande = Politician("Francois","Hollande","France",5)
+Obama = Politician("Barack", "Obama", "USA", 4)
+Merkel = Politician("Angela", "Merkel", "Germany", 6)
+Hollande = Politician("Francois", "Hollande", "France", 5)
 
 Obama.displayPolitician()
 Merkel.displayPolitician()
